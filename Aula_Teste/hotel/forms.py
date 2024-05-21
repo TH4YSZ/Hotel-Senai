ce2342892e2938a58aa27737599cadd42ac8dcc0
THAYSZ <thainacristina.sz07@gmail.com>
2024-05-21 10:16:05 -0300

    Criação das páginas de cadastro e login, e das classes e formulários para cadastro e login de usuários

--- Aula_Teste/hotel/forms.py
+++ Aula_Teste/hotel/forms.py
@@ -15,3 +15,14 @@ class FormReserva(forms.Form):
     end = forms.CharField(label="Endereço", max_length=100)
     quarto = forms.ChoiceField(label="Tipo de Quarto", choices=TIPOS_QUARTOS)
     data = forms.CharField(label="Data", max_length=8)
+
+class FormCadastro(forms.Form):
+    first_name = forms.CharField(label="Nome", max_length=20)
+    last_name = forms.CharField(label="Sobrenome", max_length=20)
+    user = forms.CharField(label="Usuário", max_length=20)
+    email = forms.EmailField(label="Email", max_length=100)
+    password = forms.CharField(label="Senha", widget=forms.PasswordInput)
+
+class FormLogin(forms.Form):
+    user = forms.CharField(label="Usuário", max_length=20)
+    password = forms.CharField(label="Senha", widget=forms.PasswordInput)
--- Aula_Teste/hotel/views.py
+++ Aula_Teste/hotel/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, HttpResponse
 from .models import *
-from .forms import FormReserva
+from .forms import *
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.conf import settings
+from django.shortcuts import redirect
 
 # Create your views here.
 def homepage(request):
@@ -18,30 +22,80 @@ def quartos(request):
     return render(request, 'quartos.html', context)
 
 def reserva(request):
-    context = {}
-    dados_hotel = Hotel.objects.all()
-    context["dados_hotel"] = dados_hotel
 
     # POST para validação
-    if request.method == "POST":
-        form = FormReserva(request.POST)
-        if form.is_valid():
-            # variavel para armazenar o que o formulário passo através do cleaned_data, que manipula o dicionário
-            var_nome = form.cleaned_data['nome']
-            var_sobrenome = form.cleaned_data['sobrenome']
-            var_email = form.cleaned_data['email']
-            var_idade = form.cleaned_data['idade']
-            var_end = form.cleaned_data['end']
-            var_quarto = form.cleaned_data['quarto']
-            var_data = form.cleaned_data['data']
-            
+    if not request.user.is_authenticated:
+        return render(request, 'login')
+    
+    else:
+        if request.method == "POST":
+            form = FormReserva(request.POST)
+            if form.is_valid():
+                # variavel para armazenar o que o formulário passo através do cleaned_data, que manipula o dicionário
+                var_nome = form.cleaned_data['nome']
+                var_sobrenome = form.cleaned_data['sobrenome']
+                var_email = form.cleaned_data['email']
+                var_idade = form.cleaned_data['idade']
+                var_end = form.cleaned_data['end']
+                var_quarto = form.cleaned_data['quarto']
+                var_data = form.cleaned_data['data']
+                
 
-            user = Reserva(nome=var_nome, sobrenome=var_sobrenome, email=var_email, idade=var_idade, end=var_end, quarto=var_quarto, data=var_data)
-            # Armazena a informação no banco
+                user = Reserva(nome=var_nome, sobrenome=var_sobrenome, email=var_email, idade=var_idade, end=var_end, quarto=var_quarto, data=var_data)
+                # Armazena a informação no banco
+                user.save()
+
+        # GET
+        else:
+            form = FormReserva()
+
+        return render(request, "reserva.html", {"form": form})
+
+
+def cadastro(request):
+
+    if request.method == "POST":
+        form = FormCadastro(request.POST)
+        if form.is_valid():
+
+            var_first_name = form.cleaned_data['first_name']
+            var_last_name = form.cleaned_data['last_name']
+            var_user = form.cleaned_data['user']
+            var_email = form.cleaned_data['email']
+            var_password = form.cleaned_data['password']
+
+            user = User.objects.create_user(username= var_user, email=var_email, password=var_password)
+            user.first_name = var_first_name
+            user.last_name = var_last_name
             user.save()
+
+            return redirect("login")
+
+
+    else:
+        form = FormCadastro()
+
+    return render(request, "cadastro.html", {"form": form})
+
+
+def login(request):
+
+    if request.method == "POST":
+        form = FormLogin(request.POST)
+        if form.is_valid():
+            
+            var_user = form.cleaned_data['user']
+            var_password = form.cleaned_data['password']
+
+            user = authenticate(username=var_user, password=var_password)
+
+            if user is not None:
+                return redirect("reserva")
+            else:
+                return redirect("login")
 
     # GET
     else:
-        form = FormReserva()
+        form = FormLogin()
 
-    return render(request, "reserva.html", {"form": form})
+    return render(request, "login.html", {"form": form})
