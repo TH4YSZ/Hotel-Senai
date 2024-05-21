318f90747ad44ed0566790177303e14add0d729a
THAYSZ <thainacristina.sz07@gmail.com>
2024-05-21 10:52:20 -0300

    Corrigindo o erro de adicionar a logo na NavBar das telas de Quartos, Reserva, Login e Cadastro

--- Aula_Teste/hotel/views.py
+++ Aula_Teste/hotel/views.py
@@ -15,91 +15,97 @@ def homepage(request):
 
 def quartos(request):
     context = {}
-    dados_quarto = Quarto.objects.all() 
+    dados_quarto = Quarto.objects.all()
+
+    dados_hotel = Hotel.objects.all() 
+    context["dados_hotel"] = dados_hotel
     
     context["dados_quarto"] = dados_quarto
     
     return render(request, 'quartos.html', context)
 
 def reserva(request):
+    context = {}
+    dados_hotel = Hotel.objects.all()
+    context["dados_hotel"] = dados_hotel
 
-    # POST para validação
     if not request.user.is_authenticated:
-        return render(request, 'login')
-    
-    else:
-        if request.method == "POST":
-            form = FormReserva(request.POST)
-            if form.is_valid():
-                # variavel para armazenar o que o formulário passo através do cleaned_data, que manipula o dicionário
-                var_nome = form.cleaned_data['nome']
-                var_sobrenome = form.cleaned_data['sobrenome']
-                var_email = form.cleaned_data['email']
-                var_idade = form.cleaned_data['idade']
-                var_end = form.cleaned_data['end']
-                var_quarto = form.cleaned_data['quarto']
-                var_data = form.cleaned_data['data']
-                
+        return render(request, 'login.html', context)
 
-                user = Reserva(nome=var_nome, sobrenome=var_sobrenome, email=var_email, idade=var_idade, end=var_end, quarto=var_quarto, data=var_data)
-                # Armazena a informação no banco
-                user.save()
+    if request.method == "POST":
+        form = FormReserva(request.POST)
+        if form.is_valid():
+            var_nome = form.cleaned_data['nome']
+            var_sobrenome = form.cleaned_data['sobrenome']
+            var_email = form.cleaned_data['email']
+            var_idade = form.cleaned_data['idade']
+            var_end = form.cleaned_data['end']
+            var_quarto = form.cleaned_data['quarto']
+            var_data = form.cleaned_data['data']
 
-                return redirect("quartos")
-            else:
-                return redirect("reserva")
-        # GET
+            user = Reserva(nome=var_nome, sobrenome=var_sobrenome, email=var_email, idade=var_idade, end=var_end, quarto=var_quarto, data=var_data)
+            user.save()
+            return redirect("quartos")
         else:
-            form = FormReserva()
+            return redirect("reserva")
+    else:
+        form = FormReserva()
 
-        return render(request, "reserva.html", {"form": form})
+    context.update({"form": form})
+    return render(request, "reserva.html", context)
+
 
 
 def cadastro(request):
+    context = {}
+    dados_hotel = Hotel.objects.all()
+    context["dados_hotel"] = dados_hotel
 
     if request.method == "POST":
         form = FormCadastro(request.POST)
         if form.is_valid():
-
             var_first_name = form.cleaned_data['first_name']
             var_last_name = form.cleaned_data['last_name']
             var_user = form.cleaned_data['user']
             var_email = form.cleaned_data['email']
             var_password = form.cleaned_data['password']
 
-            user = User.objects.create_user(username= var_user, email=var_email, password=var_password)
+            user = User.objects.create_user(username=var_user, email=var_email, password=var_password)
             user.first_name = var_first_name
             user.last_name = var_last_name
             user.save()
-
             return redirect("login")
         else:
             return redirect("cadastro")
-
     else:
         form = FormCadastro()
 
-    return render(request, "cadastro.html", {"form": form})
+    context.update({"form": form})
+    return render(request, "cadastro.html", context)
+
 
 
 def login(request):
+    context = {}
+    dados_hotel = Hotel.objects.all()
+    context["dados_hotel"] = dados_hotel
 
     if request.method == "POST":
         form = FormLogin(request.POST)
         if form.is_valid():
-            
             var_user = form.cleaned_data['user']
             var_password = form.cleaned_data['password']
 
             user = authenticate(username=var_user, password=var_password)
 
             if user is not None:
+                login(request, user)  # Log the user in
                 return redirect("quartos")
             else:
                 return redirect("login")
-
-    # GET
     else:
         form = FormLogin()
 
-    return render(request, "login.html", {"form": form})
+    context.update({"form": form})
+    return render(request, "login.html", context)
+
